448df0852cc100708e6d7ec8bad7f1bee8ed3746
yanzijie <yanzijie@sensetime.com>
2021-08-06 15:38:49 +0800

    update ohmyzsh url

--- zzohmyzsh.sh
+++ zzohmyzsh.sh
@@ -33,8 +33,8 @@ set -e
 
 # Default settings
 ZSH=${ZSH:-~/.oh-my-zsh}
-REPO=${REPO:-robbyrussell/oh-my-zsh}
-REMOTE=${REMOTE:-git@github.com:${REPO}}
+# REPO=${REPO:-robbyrussell/oh-my-zsh}
+REMOTE=${REMOTE:-https://github.com/ohmyzsh/ohmyzsh.git}
 BRANCH=${BRANCH:-master}
 
 # Other options
