4f8da6d059f3d29251b814cea7f56e16d20210a5
Yufan Zhang <zhangyufan@pjlab.org.cn>
2022-07-18 11:29:00 +0800

    fix ssh

--- zshplugin.sh
+++ zshplugin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # plugins
-git clone git@github.com:zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
-git clone git@github.com:zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
-git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
